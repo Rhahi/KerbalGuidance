32d197a3d2db311749cb18d689a5a31afdcb7f38
Rhahi <git@rhahi.com>
2022-12-07 01:11:05 +0100

    hack: apply vertical vector when velocity is low
    this is introduced to make guidance work with current implementation,
    as binary search gets confused when the rocket is slightly tilted
    towards retrograde orbit.

--- src/components/ugen.jl
+++ src/components/ugen.jl
@@ -3,14 +3,19 @@ Generate initial point, assuming positive and non-vertical velocity.
 Angle unit in degrees.
 """
 function u_generatorₗ(v, r, d, ϕ; planar_threshold=10, initial_velocity=20)
+    if norm(v) < 1
+        @info "using direction fallback"
+        # v = d/norm(d) # avoid zero velocity
+        v = r/norm(r)
     end
     if (∠θ(v, r) |> rad2deg) < planar_threshold
+        @info "using direction method"
         return θ->[initial_velocity*angle_directionₗ(v, 90-θ, ϕ); r]
     end
     return θ->[planar_rotate(v, r, -θ); r]
 end
 function u_generator(v, r, d, ϕ; planar_threshold=10)
-    if norm(v) < 0.1
+    if norm(v) < 1
         v = d/norm(d)
     end
     if (∠θ(v, r) |> rad2deg) < planar_threshold
