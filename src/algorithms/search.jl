37b2a5618b34be574f107e4cadf9391a42ea9c36
Rhahi <git@rhahi.com>
2022-12-07 01:05:37 +0100

    refactor!: make binary search more general

--- src/algorithms/search.jl
+++ src/algorithms/search.jl
@@ -1,24 +1,37 @@
 function binary_search(solver::Function, ugen::Function, evaluate::Function;
-    body, rocket, target, input_tolerance, output_tolerance, var_min, var_max, t₀, t₁
+    p=nothing, target, θ_min, θ_max, t₀, t₁,
+    search_resolution, acceptable_error_low, acceptable_error_high, returnable_error,
 )
-    @debug "begin binary search" _group=:guidance
-    var = (var_min+var_max)/2
+    @info "begin binary search" _group=:guidance
+    θ = (θ_min+θ_max)/2
+    solution = nothing
+    solution_u = nothing
+    last_solution = nothing
+    last_u = nothing
+    best_error = Inf
     while true
-        u = Vector(ugen(var))
-        error, sol = evaluate(solver(u, body, rocket, t₀, t₁), target)
-        @debug "searching... error: $(round(error)), θ: $(round(var; digits=5))" _group=:guidance
-        if abs(error) < output_tolerance
-            @debug "solution found" _group=:guidance
-            return true, sol, u
+        u = Vector(ugen(θ))
+        error, sol = evaluate(solver(u, p, t₀, t₁), target)
+        @info "searching... error: $(round(error)), θ: $(round(θ; digits=5))" _group=:guidance
+        if acceptable_error_low < error < acceptable_error_high
+            if abs(error) < abs(best_error)
+                solution = sol
+                solution_u = u
+                best_error = error
+                abs(error) < returnable_error && break
+            end
         end
-        var_min, next_var, var_max = next_binary_search_point(var_min, var, var_max, error)
-        if abs(next_var - var) < input_tolerance
-            @warn "solution not found: $(round(error)), θ: $(round(var; digits=5))" _group=:guidance
-            return false, sol, u
+        θ_min, next, θ_max = next_binary_search_point(θ_min, θ, θ_max, error)
+        if abs(next - θ) < search_resolution
+            last_solution = sol
+            last_u = u
+            break
         end
-        var = next_var
+        θ = next
         yield()
     end
+    isnothing(solution) && return false, last_solution, last_u
+    return true, solution, solution_u
 end
 
 function next_binary_search_point(var_min, var, var_max, error)
